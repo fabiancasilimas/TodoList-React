0d9d383fc78c0ce3e9860d99080f95a7b180e1fa
Fabian Casilimas <fabiancasilimas@gmail.com>
2019-03-08 12:22:38 -0500

    Navigation component

--- todolistapp/src/App.js
+++ todolistapp/src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './components/navigation';
+/*import Navigation from './components/navigation';*/
+import {ToDo} from './ToDo.json';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ToDo
+    }
+  }
   render() {
+    const ToDo = this.state.ToDo.map((ToDo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{ToDo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+              {ToDo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{ToDo.description}</p>
+              <p><mark>{ToDo.responnsible}</mark></p>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  )
     return (
       <div className="App">
-      <Navigation />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="" className="text-white">Tasks 
+            <span className="badge badge-pill badge-light ml-2">  
+              { this.state.ToDo.length }
+            </span>
           </a>
+        </nav>
+        <header className="App-header">
+        <div className="container">
+          <div className="row mt-4">
+            {ToDo}
+          </div>
+        </div>
+        
+          <img src={logo} className="App-logo" alt="logo" />
+          
         </header>
       </div>
     );
